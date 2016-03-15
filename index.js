836e813b3648107d8280702b868da8b0fbe74ed6
itezaz <ihtezaz.anwar@sharpfutures.org.uk>
2016-03-15 16:12:09 +0000

    let me join

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response){
   response.send("Route requested");
 })
 
+app.get('/itezaz', function(request, response){
+  response.send("itezaz requested");
+})
+
 app.listen(3000, function(){
   console.log("app is listening on http://localhost:3000");
 })
