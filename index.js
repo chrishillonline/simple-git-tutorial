0159318975bd9f4ba47d468182711b58c13cc2f6
JohnChorley1 <John@Johns-MacBook-Air.local>
2016-03-15 15:12:25 +0000

    Added johns route and swp to gitignore

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response){
   response.send("Route requested");
 })
 
+app.get('/john', function(request, response){
+  response.send("John Requested");
+})
+
 app.listen(3000, function(){
   console.log("app is listening on http://localhost:3000");
 })
