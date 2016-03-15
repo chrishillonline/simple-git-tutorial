7076c42a8f8cb9ac2e13abfdc25de13d11f4d5f2
Chris Hill <you@example.com>
2016-03-15 15:01:52 +0000

    Added Chris route

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(request, response){
-  response.send("Route requested");
+  response.send("Route requested")
+})
+
+app.get('/chris', function(request, response){
+  response.send("Chris requested");
 })
 
 app.listen(3000, function(){
