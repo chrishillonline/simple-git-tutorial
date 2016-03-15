38a2a430cba17897081de528477b5000fb1d0dec
Haunt3dAtk <jordon.mason@sharpfutures.org.uk>
2016-03-15 15:10:35 +0000

    changes to index file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response){
   response.send("Route requested");
 })
 
+app.get('/jordan', function(request, response){
+  response.send("jordan requested");
+})
+
 app.listen(3000, function(){
   console.log("app is listening on http://localhost:3000");
 })
